14909ab2a2c96c561066f1e394736e87177fada4
Matheus Silva de Lima <matsdelima>
2017-03-07 23:54:13 -0300

    Cadastro de cidades CONCLUIDO

--- LealBrasil/src/main/java/br/com/lealbrasil/model/entities/BaseEntity.java
+++
@@ -1,7 +0,0 @@
-package br.com.lealbrasil.model.entities;
-
-public interface BaseEntity {
-	
-	public Long getId(); //Utilizada para conversão, material utilizado: http://www.rponte.com.br/2008/07/26/entity-converters-pra-da-e-vender/
-	
-}
